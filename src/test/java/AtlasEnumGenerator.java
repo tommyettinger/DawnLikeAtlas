7102446f6e1a95ba8d4ad00646e105554158b0d8
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-09-14 02:25:11 -0700

    Add some code to generate a type-safe enum to access the TextureAtlas.

---
+++ src/test/java/AtlasEnumGenerator.java
@@ -0,0 +1,56 @@
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.Array;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Created by Tommy Ettinger on 9/14/2019.
+ */
+public class AtlasEnumGenerator extends ApplicationAdapter {
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("AtlasEnumGenerator");
+        config.setWindowedMode(500, 100);
+        config.setIdleFPS(10);
+        config.useVsync(true);
+        config.setResizable(false);
+        final AtlasEnumGenerator app = new AtlasEnumGenerator();
+        new Lwjgl3Application(app, config);
+    }
+    
+    public static String nameEnum(String baseName)
+    {
+        return baseName.toUpperCase().replace(' ', '_');
+    }
+    
+    @Override
+    public void create() {
+        TextureAtlas atlas = new TextureAtlas(Gdx.files.local("atlas/Dawnlike.atlas"));
+        //TextureAtlas.TextureAtlasData atlasData = new TextureAtlas.TextureAtlasData(Gdx.files.local("atlas/Dawnlike.atlas"), Gdx.files.local("atlas"), false);
+        Array<TextureAtlas.AtlasRegion> regions = atlas.getRegions();
+        TreeMap<String, Integer> indexCounts = new TreeMap<>();
+        TextureAtlas.AtlasRegion region;
+        for (int i = 0; i < regions.size; i++) {
+            region = regions.get(i);
+            indexCounts.put(region.name, Math.max(indexCounts.getOrDefault(region.name, -1), region.index));
+        }
+        StringBuilder sb = new StringBuilder(indexCounts.size() * 16);
+        sb.append("public enum Dawnlike {\n");
+        for (Map.Entry<String, Integer> e : indexCounts.entrySet())
+        {
+            sb.append(nameEnum(e.getKey())).append("(\"").append(e.getKey()).append("\", ").append(e.getValue() + 1 | e.getValue() >> 31).append("),\n");
+        }
+        sb.setCharAt(sb.length() - 2, ';');
+        sb.append("\nprotected Dawnlike(String name, int indices) {\n")
+                .append("  this.name = name;\n")
+                .append("  this.indices = indices;\n")
+                .append("}\n").append("}\n");
+
+        System.out.println(sb);
+    }
+}
